fb2165e0b239b090944dc3fd8f29c19025792904
xuyifan <xuyifan@gmail.com>
2025-07-26 16:32:35 +0000

    modify linux

---
+++ coperception/utils/mapping/build/CMakeFiles/mapping.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/xuyifan/v2x/coperception/coperception/utils/mapping/dynamic_library/mapping.cpp" "CMakeFiles/mapping.dir/mapping.cpp.o" "gcc" "CMakeFiles/mapping.dir/mapping.cpp.o.d"
+  "" "mapping.cpython-311-x86_64-linux-gnu.so" "gcc" "CMakeFiles/mapping.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ coperception/utils/mapping/build/CMakeFiles/mapping.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mapping.dir/link.d"
+  "CMakeFiles/mapping.dir/mapping.cpp.o"
+  "CMakeFiles/mapping.dir/mapping.cpp.o.d"
+  "mapping.cpython-311-x86_64-linux-gnu.so"
+  "mapping.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
